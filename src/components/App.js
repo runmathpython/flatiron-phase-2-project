b298b2b0ec8221bf118402b636eb06463bce82fb
Seong Kim <runmath@gmail.com>
2023-06-24 01:01:21 -0600

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState} from "react";
+import Header from "./Header";
+
+function App(){
+
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div id="movie-header">
+            <img
+                src="mvs/moviephoto.png"
+                alt="movie header"
+            />
+        </div>
+    )
+}
+
+export default Header
